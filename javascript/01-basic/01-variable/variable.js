60a0b6c9955a9ac35f8b2babb9e3409e6aef41fd
uks2101 <uks2101@naver.com>
2026-09-10 16:49:11 +0900

    [feat] javascript - variable 학습

---
+++ javascript/01-basic/01-variable/variable.js
@@ -0,0 +1,16 @@
+let age = 30;
+
+console.log(age);
+
+age = 31;
+
+console.log(age);
+
+const name = 'Kim';
+
+console.log(name);
+
+const userName = 'Kim';
+// userName = 'Lee' 안됨
+
+console.log(userName);
